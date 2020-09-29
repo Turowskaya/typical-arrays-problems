a7b1478ea3492f892020784a66a02980f847bc0f
turowskaja@gmail.com <turowskaja@gmail.com>
2020-09-29 23:43:54 +0300

    init: start task Typical Arrays Problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,32 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (array.length === 0){
+    return 0;
+  } else{
+    array.sort((a,b) => a-b);
+     return array[0];
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array.length === 0){
+    return 0;
+  } else{
+    array.sort((a,b) => a-b);
+     return array[array.length-1];
+  }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array.length === 0){
+    return 0;
+  } else{
+    let number = 0;
+
+    for(number of array){
+      let sum;
+      sum += number;
+      return sum/array.length;
+    }
+  }
 }
