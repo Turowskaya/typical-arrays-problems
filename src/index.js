4ad356061b7ae943dc61daeca06a3b9d5e591439
turowskaja@gmail.com <turowskaja@gmail.com>
2020-09-30 00:21:07 +0300

    fix: mistake in function avg

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 
 exports.min = function min (array) {
-  if (array.length === 0){
+  if (array === undefined || array[0] === undefined ){
     return 0;
   } else{
     array.sort((a,b) => a-b);
@@ -9,7 +9,7 @@ exports.min = function min (array) {
 }
 
 exports.max = function max (array) {
-  if (array.length === 0){
+  if (array === undefined || array[0] === undefined ){
     return 0;
   } else{
     array.sort((a,b) => a-b);
@@ -18,15 +18,15 @@ exports.max = function max (array) {
 }
 
 exports.avg = function avg (array) {
-  if (array.length === 0){
+  if (array === undefined || array[0] === undefined ){
     return 0;
   } else{
-    let number = 0;
+    let sum = 0;
 
-    for(number of array){
-      let sum;
-      sum += number;
-      return sum/array.length;
+    for (let i = 0; i <array.length; i++) {
+      sum += array[i];
     }
+
+    return sum/array.length;
   }
 }
